ef93fc1f88c15923b4f59645f5294865ae7b15e2
Alan Thompson <thompson2526@gmail.com>
2014-06-30 11:23:06 -0700

    Added tests for conjv

--- src/cooljure/core.clj
+++ src/cooljure/core.clj
@@ -1,9 +1,8 @@
-;   Copyright (c) Alan Thompson. All rights reserved.  ;   The use and distribution terms for this software are covered by the
-;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;   which can be found in the file epl-v10.html at the root of this distribution.
-;   By using this software in any fashion, you are agreeing to be bound by
-;   the terms of this license.
-;
+;   Copyright (c) Alan Thompson. All rights reserved.  
+;   The use and distribution terms for this software are covered by the Eclipse Public
+;   License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the
+;   file epl-v10.html at the root of this distribution.  By using this software in any
+;   fashion, you are agreeing to be bound by the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
 
 (ns ^{:doc "Cooljure - Cool stuff you wish was in Clojure"
@@ -23,19 +22,21 @@
   (if arg false true) )
 
 (defn any?
-  "Returns true if (pred x) is logical true for any x in coll; otherwise returns false.
-   Like clojure.core/some, but returns only true or false."
+  "Returns true if (pred x) is logical true for any x in collection; otherwise returns
+  false.  Like clojure.core/some, but returns only true or false."
   [pred coll]
   (truthy? (some pred coll)) )
 
 (defn not-empty?
-  "Returns false if coll contains no items; otherwise returns true.
+  "Returns true if collection contains any items; otherwise returns false
   Equivalent to (not (empty? coll))."
   [coll]
   (truthy? (seq coll)) )
 
 (defn conjv 
-  "Appends item to collection, always returning a vector."
+  "Appends items to collection, always returning the result as a vector."
   ; From Stuart Sierra post 2014-2-10
-  [coll item]
-  (conj (vec coll) item) )
+  ([coll x]
+     (conj (vec coll) x) )
+  ([coll x & xs]
+     (apply conj (vec coll) x xs) ))
--- test/cooljure/core_test.clj
+++ test/cooljure/core_test.clj
@@ -1,9 +1,8 @@
-;   Copyright (c) Alan Thompson. All rights reserved.  ;   The use and distribution terms for this software are covered by the
-;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;   which can be found in the file epl-v10.html at the root of this distribution.
-;   By using this software in any fashion, you are agreeing to be bound by
-;   the terms of this license.
-;
+;   Copyright (c) Alan Thompson. All rights reserved.  
+;   The use and distribution terms for this software are covered by the Eclipse Public
+;   License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the
+;   file epl-v10.html at the root of this distribution.  By using this software in any
+;   fashion, you are agreeing to be bound by the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
 
 (ns cooljure.core-test
@@ -36,12 +35,11 @@
 
     (testing "contrib"
       (let [ result (seq/separate boolean data) ]
-        (is (= [ [true :a 1] [false nil] ]
+        (is (= [ [true :a 1] [false nil] ]      ; corece to primitive boolean feels wrong
                result )))
       (let [ result (seq/separate truthy? data) ]
         (is (= [ [true :a 1] [false nil] ]
-               result ))))
-  ))
+               result ))))))
 
 (deftest any-tst
   (testing "basic usage"
@@ -58,3 +56,26 @@
             (map not-empty? ["1" [1] '(1) {:1 1} #{1} ] ) ))
     (is (= [false false false false false false false ]
             (map not-empty? ["" [] () '() {} #{} nil] ) ))))
+
+(deftest conjv-tst
+  (testing "basic usage"
+    (is (= [  2 3]  (conjv  []  2  3)))
+    (is (= [  2 3]  (conjv '()  2  3)))
+    (is (= [1 2 3]  (conjv  [1] 2  3)))
+    (is (= [1 2 3]  (conjv '(1) 2  3)))
+    (is (= [1 2 3]  (conjv  [1  2] 3)))
+    (is (= [1 2 3]  (conjv '(1  2) 3)))
+
+    (is (= [1 2 3 4]  (conjv  [1  2] 3 4)))
+    (is (= [1 2 3 4]  (conjv '(1  2) 3 4)))
+    (is (= [1 2 3 4]  (conjv  [1] 2  3 4)))
+    (is (= [1 2 3 4]  (conjv '(1) 2  3 4))) )
+
+  (testing "vector elements"
+    (is (=    [[1 2] [3 4]  [5 6] ]
+      (conjv '([1 2] [3 4]) [5 6] ) )))
+
+  (testing "lazy seqs/apply"
+    (is (= [0 1 2 3 4 5] (conjv (range 4) 4 5)))
+    (is (= [0 1 2 3 4 5] (apply conjv [0] (range 1 6)))) ))
+
