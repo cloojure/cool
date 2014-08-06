aa299d9f157d453384fea78cb84e05e776dfee97
Alan Thompson <cloojure@gmail.com>
2014-08-05 17:04:45 -0700

    misc

--- doLinks.zsh
+++ doLinks.zsh
@@ -1,8 +1,34 @@
 #!/bin/zsh
+scriptName=$(basename $0)
+
+usageAndExit() {
+  cat <<EOF
+
+  Usage:  cd <cool-dir>              # must first cd into ~/cool directory (or equiv)
+          ${scriptName} [--force]
+
+          Creates links symlinks for user config files pointing to files in ~/cool or
+          equiv (e.g. ~/.zshrc -> ~/cool/.zshrc).  The --force option will delete any
+          existing user config files when creating the symlinks.
+
+EOF
+exit 1
+}
+
+#************************************************************
+# main program
+
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]] ; then
+  usageAndExit
+fi
 
 forceFlg=""
-if [[ "$1" == "--force" ]] ; then
-  forceFlg="--force"
+if (( $# >=1 )) ; then
+  if [[ "$1" == "--force" ]] ; then
+    forceFlg="--force"
+  else
+    usageAndExit
+  fi
 fi
 
 coolDir=$(pwd)
