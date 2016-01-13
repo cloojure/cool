3deadc189bc29cf5a44cb344dfefcafba493f225
Alan Thompson <cloojure@gmail.com>
2016-01-12 16:27:53 -0800

    misc

--- common.bash
+++ common.bash
@@ -13,10 +13,9 @@ if [[ $(hostname) == *touchcommerce.com ]]; then
   # echo "Found touchcommerce.com"
   # sleep 1
   echo "dummy" > /dev/null  # KLUDGE: stupid bash can't handle an empty if block
-  export JAVA_HOME="~/opt/java"
+  export JAVA_HOME="/opt/jdk1.8.0_66"                 ;  path=( ${JAVA_HOME}/bin $path )
 
-  path=( ${JAVA_HOME}/bin $path )
-  path=( ~/opt/solr/bin $path )
+  # path=( ~/opt/solr/bin $path )
 else
   # echo "Not-Found touchcommerce.com"
   export JAVA_HOME="/opt/java"
