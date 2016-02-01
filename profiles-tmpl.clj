159a60b3c73105cf2c39ba1c5bfafde405459682
Alan Thompson <cloojure@gmail.com>
2016-02-01 14:48:13 -0800

    misc

--- profiles-tmpl.clj
+++ profiles-tmpl.clj
@@ -7,7 +7,7 @@
     ] 
     :dependencies [ 
       [criterium                    "0.4.3"] 
-      [org.clojure/clojure          "1.8.0-RC5"]
+      [org.clojure/clojure          "1.8.0"]
       [org.clojure/tools.namespace  "0.2.9"]
 ;     [org.clojure/tools.nrepl      "0.2.7"]
     ]
