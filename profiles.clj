b476e8e941716e967ba01e629ae51c464b2c95a6
Alan Thompson <cloojure@gmail.com>
2016-06-12 18:07:01 -0700

    misc

--- profiles.clj
+++ profiles.clj
@@ -1,17 +1,18 @@
 { :user {   
+    :dependencies [ 
+      [org.clojure/clojure          "1.9.0-alpha5"]
+;     [org.clojure/tools.namespace  "0.2.9"]
+;     [org.clojure/tools.nrepl      "0.2.7"]
+    ]
     :plugins [ 
-      [lein-ancient     "0.6.0"]
+      [lein-ancient     "0.6.0" :exclusions [org.clojure/clojure]]
+      [lein-collisions  "0.1.4"]
       [lein-exec        "0.3.6"] 
       [lein-pprint      "1.1.1"] 
       [lein-codox       "0.9.3"] 
       [jonase/eastwood  "0.2.3"] 
       [com.jakemccrary/lein-test-refresh "0.15.0"]
     ] 
-    :dependencies [ 
-      [org.clojure/clojure          "1.9.0-alpha3"]
-;     [org.clojure/tools.namespace  "0.2.9"]
-;     [org.clojure/tools.nrepl      "0.2.7"]
-    ]
 ;   :source-paths [ "/home/alan/.lein" ]
 ;   :repl-options { :init-ns user }
 }}
