f8e1ea1b735893b48f34c1154f2c999526f81c91
Alan Thompson <cloojure@gmail.com>
2016-07-15 17:08:12 -0700

    misc

--- profiles.clj
+++ profiles.clj
@@ -9,10 +9,12 @@
     ] 
     :dependencies [ 
       [org.clojure/clojure          "1.8.0"]
-;     [criterium                    "0.4.3"] 
 ;     [org.clojure/tools.namespace  "0.2.9"]
 ;     [org.clojure/tools.nrepl      "0.2.7"]
     ]
+    :test-refresh { :notify-command ["terminal-notifier" "-title" "Tests" "-message"]
+                    :quiet true
+                    :changes-only true }
 ;   :source-paths [ "/home/odl/.lein" ]
 ;   :repl-options { :init-ns user }
 ;   :deploy-repositories {  "snapshots" :clojars
