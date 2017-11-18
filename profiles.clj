e81d156dbd369dd5c52a99721fae8c9fd0b04a59
Alan Thompson <cloojure@gmail.com>
2017-11-18 13:08:45 -0800

    misc

--- profiles.clj
+++ profiles.clj
@@ -1,21 +1,12 @@
 {
   :user {
     :plugins [ 
-      [com.jakemccrary/lein-test-refresh "0.19.0"]
-      [jonase/eastwood          "0.2.3"] 
-      [lein-ancient             "0.6.0"]
-;     [lein-autoexpect          "1.9.0"]
-      [lein-codox               "0.9.3"] 
-      [lein-exec                "0.3.6"] 
-;     [lein-expectations        "0.0.8"]
-;     [lein-midje               "3.2.1"]
-;     [lein-pprint              "1.1.2"] 
+      [com.jakemccrary/lein-test-refresh "0.21.1"]
+      [jonase/eastwood          "0.2.5"] 
+      [lein-ancient             "0.6.14"]
+      [lein-codox               "0.10.3"] 
+      [lein-exec                "0.3.7"] 
     ] 
-    :dependencies [ 
-    ; [org.clojure/clojure          "1.9.0-alpha14"]
-    ; [org.clojure/clojure          "1.8.0"]
-    ; [org.clojure/clojure          "1.7.0"]
-    ]
 ;   :jvm-opts ["-Xms1g" "-Xmx4g" ] ;   "-server"
   }
 
