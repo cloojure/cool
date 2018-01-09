59fc946309879d84bc1f1516fadb619f580b405a
Alan Thompson <cloojure@gmail.com>
2018-01-09 10:40:18 -0800

    misc

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,9 @@
 {
   :user {
     :plugins [ 
-      [com.jakemccrary/lein-test-refresh "0.21.1"]
+      [com.jakemccrary/lein-test-refresh "0.22.0"]
       [jonase/eastwood          "0.2.5"] 
-      [lein-ancient             "0.6.14"]
+      [lein-ancient             "0.6.15"]
       [lein-codox               "0.10.3"] 
       [lein-exec                "0.3.7"] 
     ] 
