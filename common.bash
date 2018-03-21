eb9db34312d48a57dd769e1af0054da02d503fae
Alan Thompson <cloojure@gmail.com>
2018-03-20 21:17:57 -0700

    misc

--- common.bash
+++ common.bash
@@ -69,7 +69,7 @@ if [[ $(uname -a) =~ "Linux" ]]; then
 
 fi
 if [[ $(uname -a) =~ "Darwin" ]]; then
-  echo "Found Darwin"
+  # echo "Found Darwin"
   echo "OSX is dumb!"  > /dev/null  # stupid bash can't handle an empty "then" part
   # sleep 3
   # export DYLD_LIBRARY_PATH=/opt/oracle
