dd19a165a8db296088600363aa1f2f3922c9443d
Alan Thompson <cloojure@gmail.com>
2018-09-05 13:44:49 -0700

    misc

--- bk/go.zsh
+++ bk/go.zsh
@@ -1,7 +1,24 @@
 #!/bin/zsh 
 
-tar --create --verbose --gzip  \
-  --file /media/alan/SeagateBackup/20180905-brandy-home-alan.tar.bz2  \
+function dateTimeStr()  {
+  echo $(date --iso-8601=seconds \ | sed -e'\''s/^\(.\{19\}\)\(.*\)/\1/'\'' | sed -e'\''s/-//g'\'' | sed -e'\''s/://g'\'' | sed -e'\''s/T/-/g'\' )
+}
+
+function homeStr() {
+  echo  $(echo "${HOME}" | sed -e 's:\/:-:g' )
+}
+
+function backup-tar-name() {
+  echo "$(dateTimeStr)-$(hostname)$(homeStr).tar"
+}
+
+echo "now   => $(dateTimeStr)"
+echo "home  => $(homeStr)"
+echo "name  => $(backup-tar-name)"
+
+exit 1
+tar --create --verbose  \
+  --file "/media/alan/SeagateBackup/$(backup-tar-name)"  \
   --exclude-from ~/cool/bk/tar-excludes.txt  \
-  /home/alan
+  ${HOME}
 
