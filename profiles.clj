be5f15072e4d55c816444f53ba13df9419fdcd9f
Alan Thompson <cloojure@gmail.com>
2019-03-11 09:53:16 -0700

    misc

--- profiles.clj
+++ profiles.clj
@@ -1,19 +1,19 @@
-{
-  :user {
-    :plugins [
-      [com.jakemccrary/lein-test-refresh "0.23.0"]
-      [jonase/eastwood          "0.3.3"]
-      [lein-ancient             "0.6.15"]
-      [lein-codox               "0.10.5"]
-      [lein-environ             "1.1.0"]
-      [lein-exec                "0.3.7"]
-      [lein-pprint              "1.2.0"] ]
-    :jvm-opts ["-Xms1g" "-Xmx4g" 
-;              "-server"
-              ] 
-  } 
+{:user
+  {:plugins 
+    [
+    ;[jonase/eastwood          "0.3.5"]
+     [lein-ancient             "0.6.15"]
+    ;[lein-environ             "1.1.0"]
+     [lein-exec                "0.3.7"]
+    ;[lein-pprint              "1.2.0"] 
+     [com.jakemccrary/lein-test-refresh "0.23.0"]
+    ]
+   :jvm-opts ["-Xms1g" "-Xmx4g" 
+            ; "-server"
+             ] } 
   
-  :auth {
-    :repository-auth { #"my\.datomic\.com"    { :username "cloojure@gmail.com"
-                                                :password "9529a811-7108-4dbc-afc5-2b36608b6e82" }}}
+; :auth {
+;   :repository-auth { #"my\.datomic\.com"    { :username "cloojure@gmail.com"
+;                                               :password "9529a811-7108-4dbc-afc5-2b36608b6e82" }}}
+
 }
