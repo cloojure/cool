c2467535d7379f7edd975aa20a4207dc226b6091
Alan Thompson <cloojure@gmail.com>
2021-04-07 15:20:30 -0700

    misc

--- profiles.clj
+++ profiles.clj
@@ -2,18 +2,16 @@
   :user {
     :plugins [
       [com.jakemccrary/lein-test-refresh "0.24.1"]
-   ;  [jonase/eastwood          "0.2.11"]
+      [jonase/eastwood          "0.4.0"]
       [lein-ancient             "0.7.0"]
       [lein-codox               "0.10.7"]
       [lein-exec                "0.3.7"]]
-    :jvm-opts  ; ^:replace  ; needed to remove TieredStopAtLevel=1 default Leiningen flag
-       [ "-Xms1g" "-Xmx4g" 
-       ; "-server"
-       ]
+    :jvm-opts ["-Xms1g" "-Xmx4g" 
+;              "-server"
+              ] 
   } 
   
-; :auth {
-;   :repository-auth { #"my\.datomic\.com"    { :username "cloojure@gmail.com"
-;                                               :password "9529a811-7108-4dbc-afc5-2b36608b6e82" }}}
-
+  :auth {
+    :repository-auth { #"my\.datomic\.com"    { :username "cloojure@gmail.com"
+                                                :password "9529a811-7108-4dbc-afc5-2b36608b6e82" }}}
 }
